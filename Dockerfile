ab6641759e136cf92ae9380f570ad7ec1bc680a9
Nebula <145534960+NotNebu@users.noreply.github.com>
2025-05-19 21:28:56 +0200

    Revert "Feature/prediction error handling (#18)" (#19)
    This reverts commit b699170b2340240771ed4f1ef5fabe2007eee553.

--- Dockerfile
+++ Dockerfile
@@ -8,17 +8,8 @@ RUN npm run build
 
 # Stage 2: Serve with Nginx
 FROM nginx:alpine
-
-# Sørg for at cert-mappen findes
-RUN mkdir -p /etc/nginx/certs
-
-# Certifikater
-COPY nginx/certs/localhost.crt /etc/nginx/certs/localhost.crt
-COPY nginx/certs/localhost.key /etc/nginx/certs/localhost.key
-
-# Web og konfiguration
 COPY --from=build /app/dist /usr/share/nginx/html
 COPY nginx/default.conf /etc/nginx/conf.d/default.conf
 
-EXPOSE 443
+EXPOSE 80
 CMD ["nginx", "-g", "daemon off;"]
--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
-import fs from 'fs'
 
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    https: {
-      pfx: fs.readFileSync('./nginx/certs/localhost.key'),
-      passphrase: 'changeit',
-    },
-    port: 3000,
-    origin: 'https://localhost:3000',
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@Core': path.resolve(__dirname, './src/Core'),
-      '@Application': path.resolve(__dirname, './src/Application'),
-      '@Presentation': path.resolve(__dirname, './src/Presentation'),
-      '@Infrastructure': path.resolve(__dirname, './src/Infrastructure'),
-      '@Shared': path.resolve(__dirname, './src/Shared'),
-    },
-  },
-  build: {
-    sourcemap: true,
-  },
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: ['./vitest.setup.js'],
-    include: ["Tests/**/*.test.{jsx,js,tsx,ts}"],
-  },  
 })
